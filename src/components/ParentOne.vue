d85b356f3e4dfc7e279606c1cb4f6fda1a63f73c
Seunghoon Ko <imskojs@gmail.com>
2018-06-14 19:22:00 +0900

    sending data from parent to child through props

---
+++ src/components/ParentOne.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <child-one :title="title"></child-one>
+</div>
+</template>
+
+<script>
+import ChildOne from "@/components/ChildOne";
+export default {
+  components: {
+    ChildOne
+  },
+  data() {
+    return {
+      title: "Passed Down From Parent One"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
 <div class="home">
+  <parent-one></parent-one>
   <hello-world></hello-world>
 </div>
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld";
+import ParentOne from "@/components/ParentOne";
 export default {
   components: {
-    HelloWorld
+    HelloWorld,
+    ParentOne
   }
 };
 </script>
